217987f1037117a297b4f59a7e6a97191e94a173
usb777 <dzsamoila@gmail.com>
2021-04-28 22:06:34 -0400

    Slogan Model created

---
+++ src/com/iqenergy/model/Slogan.java
@@ -0,0 +1,62 @@
+package com.iqenergy.model;
+
+public class Slogan 
+{
+
+	private int id;	
+	private String slogan;
+	
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	public String getSlogan()
+	{
+		return slogan;
+	}
+	public void setSlogan(String slogan)
+	{
+		this.slogan = slogan;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((slogan == null) ? 0 : slogan.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Slogan other = (Slogan) obj;
+		if (id != other.id)
+			return false;
+		if (slogan == null) {
+			if (other.slogan != null)
+				return false;
+		} else if (!slogan.equals(other.slogan))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {		return "Slogan [id=" + id + ", slogan=" + slogan + "]";	}
+
+	
+	
+	
+	
+	
+	
+}
