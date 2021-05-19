7d6114140a6b5633fef9ccce712d2536f08a1179
usb777 <dzsamoila@gmail.com>
2021-05-19 12:34:57 -0400

    Email Contact Form

---
+++ src/com/iqenergy/interfaces/EmailI.java
@@ -0,0 +1,19 @@
+package com.iqenergy.interfaces;
+
+import java.util.Properties;
+
+public interface EmailI 
+{
+	
+	public static final String USERNAME = "dzendzmitry@gmail.com";
+	public static final String PASSWORD = "zpxsuvgccahddiwc";
+	
+	public static final String SMTP_GMAIL_COM = "smtp.gmail.com";
+	public static final String MAIL_SMTP_PORT = "587";
+	public static final String MAIL_SMTP_AUTH = "true";
+	public static final String MAIL_SMTP_STARTTLS_ENABLE = "true";
+   
+    
+   
+
+}
--- test/com/iqenergy/model/PageTest.java
+++ test/com/iqenergy/model/PageTest.java
@@ -33,24 +33,65 @@ public class PageTest {
 	
 	@Test
 	public void testPage_id() {
-	 page.setPage_id(1);
+	 page.setPage_id(page_id);
 	 assertEquals(page.getPage_id(), page_id);		
 	}
 	
-	/*
-	private String page_name="test page_name";
-	private String page_title="test page_title";
-	private String page_info="test page_info";
-	private int page_order=3;
-	private String page_group="test page_group";	
-	private String template="test template";
-*/
+	
+	//private String page_name="test page_name";
+	@Test
+	public void testPage_name() {
+	 page.setPage_name(page_name);
+	 assertEquals(page.getPage_name(), page_name);		
+	}
+	
+	
+	//private String page_title="test page_title";
+	@Test
+	public void testPage_title() {
+		 page.setPage_title(page_title);
+		 assertEquals(page.getPage_title(), page_title);		
+		}
+	
+	
+	//private String page_info="test page_info";
+	@Test
+	public void testPage_info() {
+		 page.setPage_info(page_info);
+		 assertEquals(page.getPage_info(), page_info);		
+		}
+	
+	//private int page_order=3;
+	@Test
+	public void testPage_order() {
+		 page.setPage_order(page_order);
+		 assertEquals(page.getPage_order(), page_order);		
+		}
+	
+	
+	//private String page_group="test page_group";	
+	@Test
+	public void testPage_group() {
+		 page.setPage_group(page_group);
+		 assertEquals(page.getPage_group(), page_group);		
+		}
+	
+	
+	
+	//private String template="test template";
+	@Test
+	public void testTemplate() {
+		 page.setTemplate(template);
+		 assertEquals(page.getTemplate(), template);		
+		}
+	
 	
 	
 	
 	
 	@After
 	public void tearDown() throws Exception {
+		page = null;
 	}
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
