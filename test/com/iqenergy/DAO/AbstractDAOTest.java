f0436b5cefc80038b69958724cae01d5138cf853
usb777 <dzsamoila@gmail.com>
2021-05-20 23:39:52 -0400

    JUNIT test

--- test/com/iqenergy/DAO/AbstractDAOTest.java
+++ test/com/iqenergy/DAO/AbstractDAOTest.java
@@ -16,21 +16,26 @@ public static 	 AbstractDAO abstractDao;
 	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+	
 
 	@Test
 	public void test() {
 		
 	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+	}
 
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
 }
